29f2df9dfe72b5f9e4b550e31c13d41f2ba040bd
Oshane Bailey <b4.oshany@gmail.com>
2021-11-30 19:15:04 +0000

    close: update payee.

--- src/Payee.php
+++ src/Payee.php
@@ -6,17 +6,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Osoobe\Utilities\Traits\Active;
+use Osoobe\Utilities\Traits\TimeDiff;
 
 class Payee extends Model
 {
     use Active;
     use HasFactory;
     use SoftDeletes;
+    use TimeDiff;
 
     protected $fillable = [
         'is_active',
         'user_id',
         'payee_id',
+        'email',
+        'phone_number',
         'service_provider',
         'status',
         'verified',
@@ -37,6 +41,20 @@ class Payee extends Model
 
     public function user() {
         return $this->belongsTo('App\Models\User', 'user_id', 'id');
+    }
+
+
+
+    /**
+     * Scope a query to only include objects  verified by email.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param string $email
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeEmail($query, string $email)
+    {
+        return $query->where('email', $email);
     }
 
 }
--- src/Traits/KyckPayeeTrait.php
+++ src/Traits/KyckPayeeTrait.php
@@ -4,19 +4,78 @@ namespace Unbank\Kyckglobal\Traits;
 
 use Osoobe\Utilities\Helpers\Str;
 use Osoobe\Utilities\Helpers\Utilities;
+use Unbank\Kyckglobal\Facades\KyckGlobal;
+use Unbank\Kyckglobal\Payee;
 
 trait KyckPayeeTrait {
 
+    /**
+     * Get the payee that owns the KyckPayeeTrait
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function payee(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo('Unbank\Kyckglobal\Payee', 'user_id');
+    }
+
+    public function getOrCreatePayee() {
+        if ( empty($this->payee) ) {
+            if ( empty($this->email) ) {
+                return null;
+            }
+
+            $user = $this;
+            $payee = Payee::where('email', $user->email)
+                ->where('user_id', null)
+                ->orWhere('user_id', $user->id)
+                ->first();
+
+            if ( ! $payee ) {
+                [$result, $payee] = KyckGlobal::createPayee($user);
+                if ( $result ) {
+                    $changes = [
+                        'email' => $user->email,
+                        'phone_number' => $user->phone_number
+                    ];
+                    activity()
+                        ->causedBy($user)
+                        ->performedOn($payee)
+                        ->withProperties($changes)
+                        ->log("Payee account was created");
+                }
+            }
+
+            if ( ! is_array($payee) ) {
+                $payee->user_id = $user->id;
+                $payee->save();
+                activity()
+                    ->causedBy($user)
+                    ->performedOn($payee)
+                    ->withProperties(['user_id' => $payee->id])
+                    ->log("Payee account info was updated");
+            } else {
+                activity()
+                    ->causedBy($user)
+                    ->withProperties($payee)
+                    ->log("Payee account info was not updated");
+                return null;
+            }
+            return $payee;
+        }
+        return $this->payee;
+    }
 
     /**
-     * Get all of the payees for the User
+     * Get all of the payees for the KyckPayeeTrait
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
     public function payees(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
-        return $this->hasMany('Unbank\Kyckglobal\Payee', 'user_id', 'id');
+        return $this->hasMany('Unbank\Kyckglobal\Payee', 'user_id');
     }
+
 
     public function kyck() {
         return $this->payees->where('service_provider', 'kyck')->first();
