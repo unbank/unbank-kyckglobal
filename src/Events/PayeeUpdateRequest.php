f55c32f1ae3ae779d610c6dee719152ed9c877b2
Oshane Bailey <b4.oshany@gmail.com>
2022-09-12 19:39:31 +0000

    close: add payee update request event

---
+++ src/Events/PayeeUpdateRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Unbank\Kyckglobal\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PayeeUpdateRequest
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+
+    public $user;
+    public $context;
+
+     /**
+     * Create a new event instance.
+      *
+      * @param \App\Models\User $user
+      * @param mixed ...$context
+      */
+    public function __construct($user, ...$context)
+    {
+        $this->user = $user;
+        $this->context = $context;
+    }
+
+}
