b76cc9375d8077130fe26f6ce90abeca4f77bcf3
heyjordn <proxima.aust@gmail.com>
2023-06-02 13:08:47 -0500

    wip, added base tests for NCR allocations

--- tests/Payee/KyckPayeeTest.php
+++ tests/Payee/KyckPayeeTest.php
@@ -10,13 +10,66 @@ class KyckPayeeTest extends TestCase {
 
     use RefreshDatabase;
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->kyckObject = new class {
+            use KyckPayeeTrait;
+        };
+
+        $this->kyckObject->payee = Payee::factory()->create();
+        $this->kyckObject->phone_number_base = "+1 876 555 5555";
+    }
+
     /**
      * @group Payee
      */
     public function test_can_use_kyc_payee_trait() {
-        $payee = Payee::factory()->create();
-        $trait = $this->getObjectForTrait(KyckPayeeTrait::class);
-        $this->assertTrue($trait->generateAllocationData());
+        $payee = $this->kyckObject->generateAllocationData();
+
+        $this->assertIsArray($payee);
+
     }
+
+    /**
+     * @group Payee
+     */
+    public function test_can_generate_ncr_allocation_using_kyc_payee_trait() {
+        $payee = $this->kyckObject->generateAllocationData();
+
+        $this->assertIsArray($payee);
+        $this->assertArrayHasKey('payeeId', $payee);
+        $this->assertArrayHasKey('contactInfo', $payee);
+        $this->assertArrayHasKey('mobile', $payee['contactInfo']);
+        $this->assertArrayHasKey('sendSMS', $payee['contactInfo']);
+        $this->assertIsArray($payee['paymentTypes']);
+        $this->assertEquals('NCRpay360', $payee['paymentTypes'][0]);
+    }
+
+    /**
+     * @group Payee
+     */
+    public function test_that_generated_ncr_allocation_is_correct() {
+        $payee = $this->kyckObject->generateAllocationData();
+        $this->assertEquals(100, $payee['ncrPay360']['ncrPay360Allocation']);
+    }
+
+    /**
+     * @group Payees
+     */
+    public function test_can_generate_ach_allocation_using_kyc_payee_trait() {
+        $payee = $this->kyckObject->generateAllocationData('ach');
+
+        $this->assertIsArray($payee);
+    }
+    /**
+     * @group Payee
+     */
+    public function test_will_return_null_if_no_payee() {
+        $trait = $this->getObjectForTrait(KyckPayeeTrait::class);
+        $this->assertEquals(null, $trait->getOrCreatePayee());
+    }
+
 
 }
