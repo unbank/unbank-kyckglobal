1e66f2e6b5a92d159a5ec363e081e3d85b659ac4
heyjordn <proxima.aust@gmail.com>
2023-06-02 12:17:06 -0500

    feat, added data to fillable

--- src/AchAccount.php
+++ src/AchAccount.php
@@ -10,12 +10,15 @@ class AchAccount extends Model {
     use HasFactory;
     use BelongsToUser;
 
+    protected $table="ach_account";
+
     protected $fillable = [
         'payee_id',
         'routing_number',
         'account_number',
         'account_name',
         'account_type',
+        'data'
     ];
 
     protected $casts = [
